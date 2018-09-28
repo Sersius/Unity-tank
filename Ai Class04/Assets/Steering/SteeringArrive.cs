8f124bb0d3b0e70e233c473fd057dd0a707102ef
Sersius <segoro97@msn.com>
2018-09-28 11:56:20 +0200

    Vamonos con el todo 3

---
+++ Ai Class04/Assets/Steering/SteeringArrive.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringArrive : MonoBehaviour {
+
+	public float min_distance = 0.1f;
+	public float slow_distance = 5.0f;
+	public float time_to_target = 0.1f;
+
+	Move move;
+
+	// Use this for initialization
+	void Start () { 
+		move = GetComponent<Move>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Steer(move.target.transform.position);
+	}
+
+	public void Steer(Vector3 target)
+	{
+		if(!move)
+			move = GetComponent<Move>();
+
+        // TODO 3: Create a vector to calculate our ideal velocity
+        // then calculate the acceleration needed to match that velocity
+        // before sending it to move.AccelerateMovement() clamp it to 
+        // move.max_mov_acceleration
+
+        Vector3 diff = move.target.transform.position - transform.position;
+
+
+        diff /= time_to_target;
+
+        if (diff.magnitude < min_distance)
+            move.AccelerateMovement(Vector3.zero);
+
+        else if (diff.magnitude <= slow_distance)
+        {
+            
+        }
+
+        diff *= move.max_mov_acceleration;
+
+        move.AccelerateMovement(diff);
+
+    }
+
+	void OnDrawGizmosSelected() 
+	{
+		// Display the explosion radius when selected
+		Gizmos.color = Color.white;
+		Gizmos.DrawWireSphere(transform.position, min_distance);
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, slow_distance);
+	}
+}
