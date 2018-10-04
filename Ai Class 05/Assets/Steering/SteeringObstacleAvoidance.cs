7b338b8954a6ecb4557a1bab9742dc02c491b6e1
Sersius <segoro97@msn.com>
2018-10-04 19:59:44 +0200

    More or less the two todos

--- Ai Class 05/Assets/Steering/SteeringObstacleAvoidance.cs
+++ Ai Class 05/Assets/Steering/SteeringObstacleAvoidance.cs
@@ -5,6 +5,7 @@ public class SteeringObstacleAvoidance : MonoBehaviour {
 
 	public LayerMask mask;
 	public float avoid_distance = 5.0f;
+    private RaycastHit hit;
 
 
 	Move move;
@@ -19,11 +20,24 @@ public class SteeringObstacleAvoidance : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		// TODO 2: Agents must avoid any collider in their way
-		// 1- Create your own (serializable) class for rays and make a public array with it
-		// 2- Calculate a quaternion with rotation based on movement vector
-		// 3- Cast all rays. If one hit, get away from that surface using the hitpoint and normal info
-		// 4- Make sure there is debug draw for all rays (below in OnDrawGizmosSelected)
+        // TODO 2: Agents must avoid any collider in their way
+        // 1- Create your own (serializable) class for rays and make a public array with it
+        // 2- Calculate a quaternion with rotation based on movement vector
+        // 3- Cast all rays. If one hit, get away from that surface using the hitpoint and normal info
+        // 4- Make sure there is debug draw for all rays (below in OnDrawGizmosSelected)    
+
+        Quaternion q = Quaternion.LookRotation(move.movement);
+
+        if (Physics.Raycast(transform.position, move.movement, out hit, mask))
+        {
+            if (hit.distance <= avoid_distance)
+            {
+                Vector3 tankmove = hit.point - hit.normal;
+                tankmove.y = 0.0f;
+                move.AccelerateMovement(tankmove);
+            }
+        }
+
 	}
 
 	void OnDrawGizmosSelected() 
@@ -34,8 +48,8 @@ public class SteeringObstacleAvoidance : MonoBehaviour {
 			float angle = Mathf.Atan2(move.movement.x, move.movement.z);
 			Quaternion q = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up);
 
-			// TODO 2: Debug draw thoise rays (Look at Gizmos.DrawLine)
-
-		}
+            // TODO 2: Debug draw thoise rays (Look at Gizmos.DrawLine)
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.white);
+        }
 	}
 }
